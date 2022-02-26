f3519bf85ee402987bac35c7ffee99b35db0241f
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-02-25 23:23:46 -0500

    fix news pagination

--- pages/api/urc/news/index.js
+++ pages/api/urc/news/index.js
@@ -1,6 +1,7 @@
 import prisma from "../../../../lib/prisma-client"
 
 export default async function handler(req, res) {
+  const { page } = req.query
   const news = await prisma.instituteNews.findMany({
     skip: 10 * page ? (page - 1) : 0,
     take: 10,
