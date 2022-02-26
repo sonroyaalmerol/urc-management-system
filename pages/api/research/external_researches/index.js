ffdc008a9a9dd7603d85cd73ba7eeb54319c3736
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-02-26 17:27:59 -0500

    better error handling

--- pages/api/research/external_researches/index.js
+++ pages/api/research/external_researches/index.js
@@ -1,7 +1,19 @@
 import prisma from "../../../../lib/prisma-client"
 
 export default async function handler(req, res) {
-  const { page, per_page } = req.query
+  let { page, per_page } = req.query
+  page = page ? parseInt(page) : 1
+  per_page = per_page ? parseInt(per_page) : 10
+
+  if (per_page < 1) {
+    res.status(400).json({
+      error: 'Invalid per_page value.',
+      page,
+      per_page,
+      total_pages
+    })
+    return;
+  }
 
   const [count, externalResearches] = await prisma.$transaction([
     prisma.externalResearch.count(),
@@ -18,10 +30,21 @@ export default async function handler(req, res) {
     })
   ])
 
+  const total_pages = Math.ceil(count / per_page)
+  if (page > total_pages || page < 1) {
+    res.status(400).json({
+      error: 'Invalid page value.',
+      page,
+      per_page,
+      total_pages,
+    })
+    return;
+  }
+
   res.status(200).json({
     data: externalResearches,
-    page: page ?? 1,
-    per_page: per_page ?? 10,
-    total_pages: Math.ceil(count / (per_page ?? 10))
+    page,
+    per_page,
+    total_pages
   })
 }
--- pages/api/research/institutes/index.js
+++ pages/api/research/institutes/index.js
@@ -1,7 +1,19 @@
 import prisma from "../../../../lib/prisma-client"
 
 export default async function handler(req, res) {
-  const { page, per_page } = req.query
+  let { page, per_page } = req.query
+  page = page ? parseInt(page) : 1
+  per_page = per_page ? parseInt(per_page) : 10
+
+  if (per_page < 1) {
+    res.status(400).json({
+      error: 'Invalid per_page value.',
+      page,
+      per_page,
+      total_pages: -1
+    })
+    return;
+  }
 
   const [count, institutes] = await prisma.$transaction([
     prisma.institute.count(),
@@ -11,10 +23,21 @@ export default async function handler(req, res) {
     })
   ])
 
+  const total_pages = Math.ceil(count / per_page)
+  if (page > total_pages || page < 1) {
+    res.status(400).json({
+      error: 'Invalid page value.',
+      page,
+      per_page,
+      total_pages
+    })
+    return;
+  }
+
   res.status(200).json({
     data: institutes,
-    page: page ?? 1,
-    per_page: per_page ?? 10,
-    total_pages: Math.ceil(count / (per_page ?? 10))
+    page,
+    per_page,
+    total_pages
   })
 }
