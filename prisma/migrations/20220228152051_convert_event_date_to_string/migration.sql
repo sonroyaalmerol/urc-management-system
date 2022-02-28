6da5876c4537ac78c54d2eea06f9c3bdb3ce9c6f
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-02-28 10:21:02 -0500

    event date to string

---
+++ prisma/migrations/20220228152051_convert_event_date_to_string/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "ResearchDissemination" ALTER COLUMN "event_date" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "ResearchPresentation" ALTER COLUMN "event_date" SET DATA TYPE TEXT;
