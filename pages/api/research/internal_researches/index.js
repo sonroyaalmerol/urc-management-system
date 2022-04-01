05ff57fb4b60abbbf6a14ef6852dad8740e2ce4a
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-03-31 22:29:47 -0400

    fix 0 data pagination

--- pages/api/research/internal_researches/index.js
+++ pages/api/research/internal_researches/index.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
   ])
   
   const total_pages = Math.ceil(count / per_page)
-  if (page > total_pages || page < 1) {
+  if (total_pages > 0 && (page > total_pages || page < 1)) {
     res.status(400).json({
       error: 'Invalid page value.',
       page,
