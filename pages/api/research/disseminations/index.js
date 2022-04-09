b4b3abffbda257112373346fc1964fde0d5304c1
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-04-08 20:14:42 -0400

    refactor collection apis

--- pages/api/research/disseminations/index.js
+++ pages/api/research/disseminations/index.js
@@ -1,47 +1,15 @@
 import prisma from "../../../../lib/prisma-client"
+import injector from "../../../../lib/injectors/collection_api"
 
 export default async function handler(req, res) {
-  let { page, per_page, search, fields } = req.query
-  page = page ? parseInt(page) : 1
-  per_page = per_page ? parseInt(per_page) : 10
-
-  if (per_page < 1) {
-    res.status(400).json({
-      error: 'Invalid per_page value.',
-      page,
-      per_page,
-      total_pages: -1
-    })
-    return;
-  }
-  let count = 0;
-  let disseminations = [];
-
-  if (search && !fields) {
-    res.status(400).json({
-      error: '"fields" selection required when searching.',
-      page,
-      per_page,
-      total_pages: 0
-    })
-    return;
-  }
-
-  try {
-    [count, disseminations] = await prisma.$transaction([
+  await injector(req, res, async ({ skip, take, where }) => {
+    return await prisma.$transaction([
       prisma.researchDissemination.count({
-        where: search ? {
-          OR: fields.split(',').map((i) => ({
-            [i]: {
-              mode: 'insensitive',
-              contains: search
-            }
-          }))
-        } : undefined
+        where
       }),
       prisma.researchDissemination.findMany({
-        skip: (per_page ?? 10) * (page ? (page - 1) : 0),
-        take: (per_page ?? 10),
+        skip,
+        take,
         include: {
           bridge_users: {
             include: {
@@ -54,41 +22,8 @@ export default async function handler(req, res) {
             }
           }
         },
-        where: search ? {
-          OR: fields.split(',').map((i) => ({
-            [i]: {
-              mode: 'insensitive',
-              contains: search
-            }
-          }))
-        } : undefined
+        where
       })
     ])
-  } catch (err) {
-    res.status(500).json({
-      error: err.message,
-      page,
-      per_page,
-      total_pages: 0
-    })
-    return;
-  }
-
-  const total_pages = Math.ceil(count / per_page)
-  if (total_pages > 0 && (page > total_pages || page < 1)) {
-    res.status(400).json({
-      error: 'Invalid page value.',
-      page,
-      per_page,
-      total_pages
-    })
-    return;
-  }
-
-  res.status(200).json({
-    data: disseminations,
-    page,
-    per_page,
-    total_pages
   })
 }
