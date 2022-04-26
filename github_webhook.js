53650b6d71f2c5f22353362a34e2fab04027565e
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-04-25 23:59:57 -0400

    modify webhook

--- github_webhook.js
+++ github_webhook.js
@@ -33,8 +33,8 @@ http
           spawnedShell.stdin.write('npx prisma migrate deploy\n');
           spawnedShell.stdin.write('npx prisma generate\n');
           spawnedShell.stdin.write('yarn build\n');
-          // spawnedShell.stdin.write('pm2 reload urc_webhook\n');
           spawnedShell.stdin.write('pm2 reload urc_ms\n');
+          spawnedShell.stdin.write('pm2 reload smee_urc\n');
           // End
           spawnedShell.stdin.end();
         } catch (error) {
