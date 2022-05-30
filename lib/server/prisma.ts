f3ff81a0657c4c3b133a31ea06bb62d2f2523e24
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-05-29 22:39:52 -0400

    revert redis

--- lib/server/prisma.ts
+++ lib/server/prisma.ts
@@ -1,13 +1,10 @@
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import slugGenerator from '../slugGenerator'
-import { createPrismaRedisCache } from "prisma-redis-middleware"
-import Redis from "ioredis"
 
 declare global {
   // allow global `var` declarations
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
-  var redis: Redis | undefined
 }
 
 export const prisma =
@@ -21,71 +18,53 @@ export const prisma =
     ]
   })
 
-export const redis =
-  global.redis ||
-  new Redis()
-
-if (process.env.NODE_ENV === 'production') {
-  prisma.$use(
-    createPrismaRedisCache({
-      storage: {
-        type: "redis",
-        options: { client: redis as any, invalidation: true },
-      },
-    }) as any
-  )
-}
-
-prisma.$use(async (params, next) => {
-  // Manipulate params here
-  const manipulatedParams = Object.assign({}, params)
-  const AUDIT_ACTIONS = [
-    'create',
-    'createMany',
-    'delete',
-    'deleteMany',
-    'update',
-    'updateMany',
-    'upsert'
-  ]
-  if (AUDIT_ACTIONS.includes(params.action) && params.model !== 'Audit') {
-    await prisma.audit.create({
-      data: {
-        table_name: params.model,
-        action: params.action,
-        args: JSON.stringify(params.args)
-      }
-    })
-
-    // add slug
-    const SLUGIFY_MODELS = [
-      'ExternalResearch',
-      'InstituteNews',
-      'ResearchPresentation',
-      'ResearchDissemination',
-      'JournalPublication',
-      'BookPublication',
-      'URCFundedResearch'
+  prisma.$use(async (params, next) => {
+    // Manipulate params here
+    const manipulatedParams = Object.assign({}, params)
+    const AUDIT_ACTIONS = [
+      'create',
+      'createMany',
+      'delete',
+      'deleteMany',
+      'update',
+      'updateMany',
+      'upsert'
     ]
-    if (SLUGIFY_MODELS.includes(params.model)) {
-      if (params.action === 'create') {
-        if (!params.args.data.slug && params.args.data.title) {
-          manipulatedParams.args.data.slug = slugGenerator(params.args.data.title)
+    if (AUDIT_ACTIONS.includes(params.action) && params.model !== 'Audit') {
+      await prisma.audit.create({
+        data: {
+          table_name: params.model,
+          action: params.action,
+          args: JSON.stringify(params.args)
         }
-      } else if (params.action === 'upsert') {
-        if (!params.args.create.slug && params.args.create.title) {
-          manipulatedParams.args.create.slug = slugGenerator(params.args.create.title)
+      })
+
+      // add slug
+      const SLUGIFY_MODELS = [
+        'ExternalResearch',
+        'InstituteNews',
+        'ResearchPresentation',
+        'ResearchDissemination',
+        'JournalPublication',
+        'BookPublication',
+        'URCFundedResearch'
+      ]
+      if (SLUGIFY_MODELS.includes(params.model)) {
+        if (params.action === 'create') {
+          if (!params.args.data.slug && params.args.data.title) {
+            manipulatedParams.args.data.slug = slugGenerator(params.args.data.title)
+          }
+        } else if (params.action === 'upsert') {
+          if (!params.args.create.slug && params.args.create.title) {
+            manipulatedParams.args.create.slug = slugGenerator(params.args.create.title)
+          }
         }
       }
+
     }
+    const result = await next(manipulatedParams)
+    // See results here
+    return result
+  })
 
-  }
-  const result = await next(manipulatedParams)
-  // See results here
-  return result
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
-  global.redis = redis
-}
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
